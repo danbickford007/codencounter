28c8232b2a37589c40bd2225adf54afa06b9e1df
Dan Bickford <danbickford007@yahoo.com>
2013-07-07 16:29:48 -0400

    ignore and rvmrc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codencounter::Application.config.secret_key_base = '0ea45bd370730ed1bb8508df9141133c680f9ee93a20bee31630b7363c8c229535979c4d4aa43f57f452ff6e0328535f2190a455e53b79088cd13b9b7d3a74e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Codencounter::Application.config.session_store :cookie_store, key: '_codencounter_session'
